201fdee4d1a28e8b3cbf68df0a3730703865e691
Ishan <ishanpahwa14@gmail.com>
2022-04-30 19:07:26 +0200

    Updated frontend

--- src/main/java/com/example/travelplanning/TravelForm.java
+++ src/main/java/com/example/travelplanning/TravelForm.java
@@ -5,6 +5,18 @@ public class TravelForm {
 	public int numPeople;
 	public Climate climate;
 
+	public void setBudget(float budget) {
+		this.budget = budget;
+	}
+
+	public void setClimate(Climate climate) {
+		this.climate = climate;
+	}
+
+	public void setNumPeople(int numPeople) {
+		this.numPeople = numPeople;
+	}
+
 	public TravelForm() {
 		budget = 0.0f;
 		numPeople = 0;
