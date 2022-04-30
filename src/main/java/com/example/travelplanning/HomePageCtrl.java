f4aea3452850cc8ff8a8642cb2005264e2e7c8e8
Ishan <ishanpahwa14@gmail.com>
2022-04-30 16:54:28 +0200

    Adding skeleton for frontend

--- src/main/java/com/example/travelplanning/HomePageCtrl.java
+++ src/main/java/com/example/travelplanning/HomePageCtrl.java
@@ -1,11 +1,15 @@
 package com.example.travelplanning;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HomePageCtrl {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Button Plan;
 
     @FXML
     protected void onHelloButtonClick() {
