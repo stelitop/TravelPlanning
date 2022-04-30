2e4af35efc2c2a8820fc1847ec58e72c2a983fc2
Ishan <ishanpahwa14@gmail.com>
2022-04-30 18:10:28 +0200

    Working on frontend

--- src/main/java/com/example/travelplanning/Controllers/HomePageCtrl.java
+++ src/main/java/com/example/travelplanning/Controllers/HomePageCtrl.java
@@ -14,6 +14,7 @@ public class HomePageCtrl extends MainCtrl {
     @FXML
     protected void onPlanButtonPressed() {
         System.out.println("Pressed plan button");
+
     }
 
 }
--- src/main/java/com/example/travelplanning/Controllers/MainCtrl.java
+++ src/main/java/com/example/travelplanning/Controllers/MainCtrl.java
@@ -4,6 +4,10 @@ import javafx.scene.Scene;
 
 public class MainCtrl {
 
+    private Scene Questions;
+
+    private Scene Results;
+
     public void goToPage(Scene scene){
 
     }
